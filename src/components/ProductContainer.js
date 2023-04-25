20c0fa8e3d7c0ecf7164f7935a33e44a1ce4b957
Yash Gorasia <109784551+yashgo09@users.noreply.github.com>
2023-04-25 09:30:34 +0530

    Fetch data from URL

--- src/components/ProductContainer.js
+++ src/components/ProductContainer.js
@@ -1,29 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
-import allProducts from "../data";
+// import allProducts from "../data";
 
 function ProductContainer({ searchQuery }) {
-  const productsList = allProducts.filter((product) => {
+  const [allProducts, setAllProducts] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  let productsList;
+
+  const fetchData = () => {
+    fetch("https://dummyjson.com/products")
+      .then((data) =>
+        data.json().then((jsonData) => {
+          console.log(jsonData.products);
+          setAllProducts(jsonData.products);
+        })
+      )
+      .catch((error) => console.error(error));
+  };
+
+  productsList = allProducts.filter((product) => {
     const title = product.title.toLowerCase();
     if (searchQuery) {
-      return title.includes(searchQuery);
+      return title.includes(searchQuery.toLowerCase());
     }
     return product;
   });
+
   return (
     <section className="product-container">
-      {productsList.map((product) => (
-        <ProductCard
-          key={product.id}
-          title={product.title}
-          description={product.description}
-          imageURL={product.images[0]}
-          price={product.price}
-          rating={product.rating}
-          stock={product.stock}
-          discount={product.discountPercentage}
-        />
-      ))}
+      {productsList &&
+        productsList.map((product) => (
+          <ProductCard
+            key={product.id}
+            title={product.title}
+            description={product.description}
+            imageURL={product.images[0]}
+            price={product.price}
+            rating={product.rating}
+            stock={product.stock}
+            discount={product.discountPercentage}
+          />
+        ))}
     </section>
   );
 }
