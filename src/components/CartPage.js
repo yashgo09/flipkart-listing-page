e96a60a41790bb6a01611e5a92e2e58bba263b97
Yash Gorasia <109784551+yashgo09@users.noreply.github.com>
2023-05-18 00:24:30 +0530

    Quantity increase decrease buttons working

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -1,23 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import CartProductCard from "./CartProductCard";
 import { CartContext } from "../Contexts";
 
 function CartPage() {
   const { cartItems } = useContext(CartContext);
-  // console.log("Inside CartPage ", cartItems);
+  const [cartPrice, setCartPrice] = useState(0);
+
   return (
     <main className="cart-page">
       <section className="cart-products">
         <h2 className="cart-products__title">My Cart ({cartItems.length})</h2>
         {/* <CartProductCard /> */}
         {cartItems.map((item) => (
-          <CartProductCard key={item.id} productDetails={item} />
+          <CartProductCard key={item.id} productDetails={item} setCartPrice={setCartPrice} />
         ))}
       </section>
       <aside className="price-details">
         <h2 className="price-details__title">Price Details</h2>
         <p>
-          Price (1 items) <span>$22.0</span>
+          Price (1 items) <span>${cartPrice}</span>
         </p>
         <p>
           Discount <span>-$1.0</span>
--- src/components/CartProductCard.js
+++ src/components/CartProductCard.js
@@ -2,12 +2,31 @@ import React, { useState } from "react";
 import Button from "./Button";
 import RemoveFromCartButton from "./RemoveFromCartButton";
 
-function CartProductCard({ productDetails }) {
+function CartProductCard({ productDetails, setCartPrice }) {
   const { id, title, imageURL, discount, price } = productDetails;
 
   const [quantity, setQuantity] = useState(1);
+  if (quantity < 1) setQuantity(1);
 
-  console.log(quantity);
+  const discountInCurrency = Math.round(price * discount) / 100;
+  const discountedPrice = price - discountInCurrency;
+
+  const totalProductPrice = discountedPrice * quantity;
+  const totalProductDiscount = discountInCurrency * quantity;
+
+  // setCartPrice(totalProductPrice);
+
+  const increaseQuantity = () => {
+    setQuantity((prev) => prev + 1);
+  };
+
+  const decreaseQuantity = () => {
+    setQuantity((prev) => prev - 1);
+  };
+
+  const handleChange = (e) => {
+    setQuantity(parseInt(e.target.value));
+  };
 
   return (
     <div className="cart-products__card">
@@ -18,9 +37,9 @@ function CartProductCard({ productDetails }) {
         <p className="cart-products__price">${price}</p>
         <div className="cart-products__controls">
           <div className="cart-products__controls--quantity">
-            <Button text="-" type="round" />
-            <input type="number" min="1" onChange={(e) => setQuantity(e.target.value)} />
-            <Button text="+" type="round" />
+            <Button text="-" type="round" clickHandler={decreaseQuantity} />
+            <input type="number" min="1" value={quantity} onChange={handleChange} />
+            <Button text="+" type="round" clickHandler={increaseQuantity} />
           </div>
           <Button text="Save For Later" type="default" />
           <RemoveFromCartButton productId={id} />
